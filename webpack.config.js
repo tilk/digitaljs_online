97bb5de9a52b67344d4eec5c9e381d0816674ef3
Marek Materzok <tilk@tilk.eu>
2021-08-11 17:09:39 +0200

    CSS extract for production builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,69 +2,74 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const outputDirectory = "dist";
 
-module.exports = {
-    entry: "./src/client/index.js",
-    devtool: "source-map",
-    output: {
-        path: path.join(__dirname, outputDirectory),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mjs/,
-                use: {
-                    loader: "babel-loader",
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== "production";
+    return {
+        entry: "./src/client/index.js",
+        devtool: "source-map",
+        output: {
+            path: path.join(__dirname, outputDirectory),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mjs/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            plugins: [
+                                "@babel/plugin-proposal-class-properties"
+                            ]
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader"]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [devMode ? "style-loader" : MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+                },
+                {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                    type: 'asset'
+                },
+                {
+                    test: require.resolve('jquery'),
+                    loader: 'expose-loader',
                     options: {
-                        plugins: [
-                            "@babel/plugin-proposal-class-properties"
-                        ]
+                        exposes: ['$']
                     }
                 }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"]
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-                type: 'asset'
-            },
-            {
-                test: require.resolve('jquery'),
-                loader: 'expose-loader',
-                options: {
-                    exposes: ['$']
-                }
-            }
-        ]
-    },
-    devServer: {
-        port: 3000,
-        open: true,
-        proxy: {
-            "/api": "http://localhost:8080"
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            inject: 'head'
-//            favicon: "./public/favicon.ico"
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'public/*.+(ico|png|svg|webmanifest)', to: '[name][ext]' },
-                { from: 'node_modules/yosys2digitaljs/tests/*.sv', to: 'examples/[name][ext]' }
             ]
-        })
-    ]
+        },
+        devServer: {
+            port: 3000,
+            open: true,
+            proxy: {
+                "/api": "http://localhost:8080"
+            }
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+                inject: 'head'
+    //            favicon: "./public/favicon.ico"
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'public/*.+(ico|png|svg|webmanifest)', to: '[name][ext]' },
+                    { from: 'node_modules/yosys2digitaljs/tests/*.sv', to: 'examples/[name][ext]' }
+                ]
+            })
+        ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    };
 };
+
